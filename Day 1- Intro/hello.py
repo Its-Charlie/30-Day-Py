9a408e34266fa4d6e45f727e426fc5f2e3a40d25
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-16 13:42:52 +0530

    Done with print Function and Data Types

--- Day 1- Intro/hello.py
+++ Day 1- Intro/hello.py
@@ -1,7 +1,7 @@
 # Introduction
 print("Hello, World!") #print statement to display "Hello, World!"
 
-# Arithematic Operations:
+# Simple Math Operations:
 print(3 + 2)  # Addition(+)
 print(3 - 2) # Subtraction(-)
 print(3 * 2)  # Multiplication(*)
@@ -9,3 +9,13 @@ print(3 / 2) # Division(/)
 print(3 ** 2) # Exponentiation(**)
 print(3 % 2) # Modulus(%)
 print(3 // 2) # Floor Division(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Tuple
