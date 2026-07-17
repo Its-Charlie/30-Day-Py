85feed8eb7b584abbf222c989fe593085e7eb820
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-17 15:23:20 +0530

    Check the data types by using type()

--- Day 2- Buildin functions and Variables/Builtinfunc.py
+++ Day 2- Buildin functions and Variables/Builtinfunc.py
@@ -7,3 +7,13 @@ city = "Kamothe"
 age = 22
 year = 2026
 is_married = "Single"
+
+# Check the data types of the variables:
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
