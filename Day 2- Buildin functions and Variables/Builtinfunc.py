dec1c401d75c526df9c99c973839737416935ac2
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-17 15:20:46 +0530

    Declare variables

--- Day 2- Buildin functions and Variables/Builtinfunc.py
+++ Day 2- Buildin functions and Variables/Builtinfunc.py
@@ -1,1 +1,9 @@
-print(len("H"))
+# Declare variables:
+first_name = "Siddhesh"
+last_name = "Yadav"
+full_name = print(first_name, last_name)
+country = "India"
+city = "Kamothe"
+age = 22
+year = 2026
+is_married = "Single"
