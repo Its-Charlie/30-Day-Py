b30b2df4f2bf895ed8101bf787c0011a531c8db6
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-17 15:31:54 +0530

    used len() in variables

--- Day 2- Buildin functions and Variables/Builtinfunc.py
+++ Day 2- Buildin functions and Variables/Builtinfunc.py
@@ -17,3 +17,9 @@ print(type(city))
 print(type(age))
 print(type(year))
 print(type(is_married))
+
+# Use the len() method to determine the length of a variable:
+print(len(first_name)) #show length of first_name variable
+print(len(last_name)) #show length of last_name variable
+print(min(first_name, last_name)) #compare and show the length of the smaller one
+print(max(first_name, last_name)) #compare and show the length of the larger one
