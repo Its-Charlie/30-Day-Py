c414e6d889e93a8899495169b9d4c251fd63d7d7
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-18 14:24:47 +0530

    Using OR operator

--- Day 3- Operators/Logical Operator.py
+++ Day 3- Operators/Logical Operator.py
@@ -2,3 +2,8 @@
 print(3 > 2 and 4 > 3) # True 
 print(3 > 2 and 4 < 3) # False
 print(3 < 2 and 4 < 3) # False
+
+# OR operator:
+print(3 > 2 or 4 > 3)  # True 
+print(3 > 2 or 4 < 3)  # True 
+print(3 < 2 or 4 < 3)  # False
