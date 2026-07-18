660f67375d9698041f1ed9125df0fff49585f5e1
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-18 14:15:57 +0530

    Using comparison operator to compare lenght of strings

--- Day 3- Operator/Comparison Operator.py
+++ Day 3- Operator/Comparison Operator.py
@@ -7,6 +7,7 @@ print(2 <= 3)    # True, because 2 is less than 3
 print(3 == 2)    # False, because 3 is not equal to 2
 print(3 != 2)    # True, because 3 is not equal to 2
 
+
 #using comparison operators to compare strings length
 print(len('mango') == len('avocado'))  # False
 print(len('mango') != len('avocado'))  # True
@@ -15,4 +16,3 @@ print(len('milk') != len('meat'))      # False
 print(len('milk') == len('meat'))      # True
 print(len('tomato') == len('potato'))  # True
 print(len('python') > len('dragon'))   # False
-
