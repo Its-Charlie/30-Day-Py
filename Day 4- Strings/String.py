4adeb51ae94b6beaca58519aa6d50ec1579594c1
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-21 16:38:02 +0530

    String Concatenation

--- Day 4- Strings/String.py
+++ Day 4- Strings/String.py
@@ -4,3 +4,11 @@ last_name = "Yadav"
 
 print(first_name)
 print(last_name)
+
+#String Concatenation:
+first_name = "Siddhesh"
+last_name = "Yadav"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
