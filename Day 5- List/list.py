3428f07a0637122b948c4647ebc6ec780b8e0cfa
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-22 15:46:14 +0530

    Create list and identing elements

---
+++ Day 5- List/list.py
@@ -0,0 +1,9 @@
+#Creating a List
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+#Accessing List Elements
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
