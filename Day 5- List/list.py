d94a2d784bbbfd80e7b06cb8c75b1645d7dad270
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-22 15:50:34 +0530

    Change and added element in list

--- Day 5- List/list.py
+++ Day 5- List/list.py
@@ -18,3 +18,13 @@ numbers = [10, 20, 30, 40, 50]
 print(numbers[1:4])
 print(numbers[:3])
 print(numbers[2:])
+
+#modifying List Elements:
+
+fruits = ["Apple", "Banana", "Mango"]
+fruits[1] = "Orange"
+print(fruits)
+
+fruits = ["Apple", "Banana"]
+fruits.append("Mango")
+print(fruits)
