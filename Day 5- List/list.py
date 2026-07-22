c3e6eb5d64d5b2c5b4087007a00c83bba0690021
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-22 15:48:09 +0530

    Trying negative indexing and list slicing

--- Day 5- List/list.py
+++ Day 5- List/list.py
@@ -7,3 +7,14 @@ fruits = ["Apple", "Banana", "Mango"]
 print(fruits[0])
 print(fruits[1])
 print(fruits[2])
+
+#Negative Indexing
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits[-1])
+print(fruits[-2])
+
+#List Slicing
+numbers = [10, 20, 30, 40, 50]
+print(numbers[1:4])
+print(numbers[:3])
+print(numbers[2:])
