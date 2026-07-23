f27057eaa11825ddb851ac8f4e7789cbefb7cbd1
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-23 19:52:27 +0530

    Tuple with different data types and accessing using index

--- Day 6- Tuple/Tuple.py
+++ Day 6- Tuple/Tuple.py
@@ -1,3 +1,13 @@
 #Creating a Tuple
 fruits = ("Apple", "Banana", "Mango")
 print(fruits)
+
+#Tuple with Different Data Types
+data = ("Siddhesh", 22, 75.5, True)
+print(data)
+
+#Accessing Tuple Elements
+fruits = ("Apple", "Banana", "Mango")
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
