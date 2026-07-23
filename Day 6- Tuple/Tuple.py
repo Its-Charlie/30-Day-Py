8cea028b8b6ac19b56afd326e2ba533614e53c1e
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-23 19:59:40 +0530

    Joining two tuples and repetition of elements

--- Day 6- Tuple/Tuple.py
+++ Day 6- Tuple/Tuple.py
@@ -34,3 +34,17 @@ print(numbers.count(2))
 fruits = ("Apple", "Banana", "Mango")
 
 print(fruits.index("Banana"))
+
+
+#Tuple Concatenation
+tuple1 = (1, 2)
+tuple2 = (3, 4)
+
+tuple3 = tuple1 + tuple2
+
+print(tuple3)
+
+#Tuple Repetition
+numbers = (1, 2)
+
+print(numbers * 3)
