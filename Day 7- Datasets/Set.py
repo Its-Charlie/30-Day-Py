1563c3f64a4c0d6385c8ae0c88978dffdae918ca
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-24 14:12:19 +0530

    used len()

--- Day 7- Datasets/Set.py
+++ Day 7- Datasets/Set.py
@@ -4,3 +4,6 @@ it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Am
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+#Using len() to determine the length of the set:
+print(len(it_companies))  # Output: 7
