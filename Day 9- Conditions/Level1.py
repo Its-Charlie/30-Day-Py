075541d5c74387aaabdd2a662a1297fdcc0c5fa7
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:52:18 +0530

    Solve Q3 of level 1

--- Day 9- Conditions/Level1.py
+++ Day 9- Conditions/Level1.py
@@ -36,3 +36,20 @@ elif your_age < my_age:
         print(f"I am {diff} years older than you.")
 else:
     print("We are the same age.")
+
+
+'''Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. Output:
+
+Enter number one: 4
+Enter number two: 3
+4 is greater than 3'''
+
+a = int(input("Enter number one: "))
+b = int(input("Enter number two: "))
+
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{a} is smaller than {b}")
+else:
+    print(f"{a} is equal to {b}")
