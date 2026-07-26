ca27a1fe599d59ef385473fff19ebc50ddca9287
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:56:48 +0530

    Solve Q2 of level 2

--- Day 9- Conditions/Level2.py
+++ Day 9- Conditions/Level2.py
@@ -18,3 +18,20 @@ elif 60 <= score <= 69:
     print("Grade D")
 else:
     print("Grade F")
+
+'''Get the month from user input then check if the season is Autumn, Winter, Spring or
+Summer. If the user input is: September, October or November, the season is Autumn.
+December, January or February, the season is Winter. March, April or May, the season is
+Spring June, July or August, the season is Summer'''
+month = input("Enter month: ").lower()
+
+if month in ["september", "october", "november"]:
+    print("Autumn")
+elif month in ["december", "january", "february"]:
+    print("Winter")
+elif month in ["march", "april", "may"]:
+    print("Spring")
+elif month in ["june", "july", "august"]:
+    print("Summer")
+else:
+    print("Invalid month")
