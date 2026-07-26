82f806982d7e9ecbdf806e377e3fd507be584312
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:51:29 +0530

    Solve Q2 of level 1

--- Day 9- Conditions/Level1.py
+++ Day 9- Conditions/Level1.py
@@ -12,3 +12,27 @@ if age >= 18:
     print("You are old enough to learn to drive.")
 else:
     print(f"You need {18 - age} more years to learn to drive.")
+
+
+'''Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
+
+Enter your age: 30
+You are 5 years older than me.'''
+
+my_age = 25
+your_age = int(input("Enter your age: "))
+
+if your_age > my_age:
+    diff = your_age - my_age
+    if diff == 1:
+        print("You are 1 year older than me.")
+    else:
+        print(f"You are {diff} years older than me.")
+elif your_age < my_age:
+    diff = my_age - your_age
+    if diff == 1:
+        print("I am 1 year older than you.")
+    else:
+        print(f"I am {diff} years older than you.")
+else:
+    print("We are the same age.")
