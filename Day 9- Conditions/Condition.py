39dee6e41baff367da54cd2557e54fe1c2cb9908
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:41:32 +0530

    Perform if-elif-else condition example

--- Day 9- Conditions/Condition.py
+++ Day 9- Conditions/Condition.py
@@ -10,3 +10,12 @@ if a < 0:
     print('A is a negative number')
 else:
     print('A is a positive number')
+
+#If-elif-else condition:
+a = 0
+if a > 0:
+    print('A is a positive number')
+elif a < 0:
+    print('A is a negative number')
+else:
+    print('A is zero')
