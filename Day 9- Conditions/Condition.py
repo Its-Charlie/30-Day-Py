23a4ae739d80c3f79bfcd1020015d492bd11ff6f
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:43:50 +0530

    Perform If Condition and Logical Operators  example

--- Day 9- Conditions/Condition.py
+++ Day 9- Conditions/Condition.py
@@ -32,3 +32,13 @@ elif a == 0:
 else:
     print('A is a negative number')
 
+#If condition with logical operators:
+a = 0
+if a > 0 and a % 2 == 0:
+        print('A is an even and positive integer')
+elif a > 0 and a % 2 !=  0:
+     print('A is a positive integer')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is negative')
