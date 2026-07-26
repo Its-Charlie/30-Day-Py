ed05044c2857a4bc0d5e52a22dff896fbf59fe06
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:42:52 +0530

    Perform nested-if condition example

--- Day 9- Conditions/Condition.py
+++ Day 9- Conditions/Condition.py
@@ -19,3 +19,16 @@ elif a < 0:
     print('A is a negative number')
 else:
     print('A is zero')
+
+##Nested if condition:
+a = 0
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
+
