3ae4393905f7de16eba21879a8e71eb0e280eaf7
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-26 13:40:27 +0530

    Perform if-else condition example

--- Day 9- Conditions/Condition.py
+++ Day 9- Conditions/Condition.py
@@ -3,3 +3,10 @@ a = 3
 if a > 0:
     print('A is a positive number')
 # A is a positive number
+
+##If-else condition:
+a = 3
+if a < 0:
+    print('A is a negative number')
+else:
+    print('A is a positive number')
