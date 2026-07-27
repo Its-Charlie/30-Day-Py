014d91efd98c4d65b3e8273ac10c4ebf9c7431da
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:44:55 +0530

    For Loop

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -34,3 +34,9 @@ while count < 5:
         continue
     print(count)
     count = count + 1
+
+#For Loop:
+print("For Loop:")
+numbers = [0, 1, 2, 3, 4, 5]
+for number in numbers: # number is temporary name to refer to the list's items, valid only inside this loop
+    print(number)       # the numbers will be printed line by line, from 0 to 5
