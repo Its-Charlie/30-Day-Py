40f9957ce78176ef15916a7e8f029f19ba928597
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:53:19 +0530

    Using For loop with pass

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -100,3 +100,8 @@ for number in numbers:
         continue
     print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
 print('outside the loop')
+
+#Using For Loop with pass:
+print("Using For Loop with pass:")
+for number in range(6):
+    pass
