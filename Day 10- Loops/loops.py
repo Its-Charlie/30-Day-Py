6926d634ddd78756dbd04b8875cd9e270c99b8e3
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:54:10 +0530

    using Range function

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -105,3 +105,19 @@ print('outside the loop')
 print("Using For Loop with pass:")
 for number in range(6):
     pass
+
+#Range Function:
+print("Range Function:")
+lst = list(range(11))
+print(lst) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+st = set(range(1, 11))    # 2 arguments indicate start and end of the sequence, step set to default 1
+print(st) # {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+lst = list(range(0,11,2))
+print(lst) # [0, 2, 4, 6, 8, 10]
+st = set(range(0,11,2))
+print(st) #  {0, 2, 4, 6, 8, 10}
+
+# for backward from start to end 
+lst = list(range(11,0,-2))
+print(lst) # [11,9,7,5,3,1]
