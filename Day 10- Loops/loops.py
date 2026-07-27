f3c36489f5affd725f0a83b5d0cf9843db69304d
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:31:47 +0530

    While loop using else

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -3,4 +3,11 @@ count = 0
 while count < 5:
     print(count)
     count = count + 1
-#prints from 0 to 4
+
+#while Loop with else:
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+else:
+    print(count)
