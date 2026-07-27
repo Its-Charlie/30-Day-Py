267c1cfd9d63643c926647637dc2066997f25e8e
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:52:37 +0530

    Using For loop with continue

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -90,3 +90,13 @@ for number in numbers:
     print(number)
     if number == 3:
         break
+
+#Using For Loop with continue:
+print("Using For Loop with continue:")
+numbers = (0,1,2,3,4,5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ', number + 1) if number != 5 else print("loop's end") # for short hand conditions need both if and else statements
+print('outside the loop')
