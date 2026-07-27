30dd3be209238a8f227fca64b2303a280e76aea4
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:34:01 +0530

    While loop with break

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -11,3 +11,11 @@ while count < 5:
     count = count + 1
 else:
     print(count)
+
+#while Loop with break:
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+    if count == 3:
+        break
