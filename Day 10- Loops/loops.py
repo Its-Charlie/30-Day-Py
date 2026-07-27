65f5531322f4b9fd12b8ce565cdccf6b1b855c4e
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-27 22:43:49 +0530

    While Loop with continue

--- Day 10- Loops/loops.py
+++ Day 10- Loops/loops.py
@@ -1,21 +1,36 @@
 #While Loop:
+print("While Loop:")
 count = 0
 while count < 5:
-    print(count)
+    print(count, end=" ")
     count = count + 1
+    print()
 
 #while Loop with else:
+print("While Loop with else:")
 count = 0
 while count < 5:
-    print(count)
+    print(count,sep="\n")
     count = count + 1
 else:
     print(count)
 
+
 #while Loop with break:
+print("While Loop with break:")
 count = 0
 while count < 5:
     print(count)
     count = count + 1
     if count == 3:
         break
+
+#while Loop with continue:
+print("While Loop with continue:")
+count = 0
+while count < 5:
+    if count == 3:
+        count += 1
+        continue
+    print(count)
+    count = count + 1
