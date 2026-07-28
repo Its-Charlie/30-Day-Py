4bc8706025bd6afbb681f72c4f7d4ed6705ca12e
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-28 15:42:08 +0530

    Function using return keyword

--- Day 11- Functions/function.py
+++ Day 11- Functions/function.py
@@ -14,3 +14,19 @@ def add_two_numbers ():
     total = num_one + num_two
     print(total)
 add_two_numbers()
+
+#Function with Returning a Value:
+def generate_full_name ():
+    first_name = 'Asabeneh'
+    last_name = 'Yetayeh'
+    space = ' '
+    full_name = first_name + space + last_name
+    return full_name
+print(generate_full_name())
+
+def add_two_numbers ():
+    num_one = 2
+    num_two = 3
+    total = num_one + num_two
+    return total
+print(add_two_numbers())
