af7346c575a9351a7644665728de026c2b669436
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-29 21:57:20 +0530

    User ID Generator

--- Day 12- Modules/level1.py
+++ Day 12- Modules/level1.py
@@ -12,3 +12,21 @@ def random_user_id():
     return user_id
 
 print(random_user_id())
+
+#User ID Generator
+import random
+import string
+
+def user_id_gen_by_user():
+    length = int(input("Enter number of characters: "))
+    count = int(input("Enter number of IDs: "))
+
+    characters = string.ascii_letters + string.digits
+
+    for i in range(count):
+        user_id = ""
+        for j in range(length):
+            user_id += random.choice(characters)
+        print(user_id)
+
+user_id_gen_by_user()
