3cb9094db5035407118a6678d58997bce93bf418
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-29 21:53:17 +0530

    Import modules with different ways

---
+++ Day 12- Modules/Example.py
@@ -0,0 +1,4 @@
+#Now import the module and use the function:
+import math #import functions from the math module
+from math import sqrt, pi #import specific functions from the math module
+from math import * #import all functions from the math module
