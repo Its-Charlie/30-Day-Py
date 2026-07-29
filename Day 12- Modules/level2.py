81018c2457a0564306c96fa75664b000a6fb463f
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-29 22:01:11 +0530

    generate colors

--- Day 12- Modules/level2.py
+++ Day 12- Modules/level2.py
@@ -31,3 +31,27 @@ def list_of_rgb_colors(n):
 
 print(list_of_rgb_colors(5))
 
+
+#Generate Colors
+import random
+
+def generate_colors(color_type, n):
+    colors = []
+
+    if color_type == "hexa":
+        for i in range(n):
+            color = "#"
+            for j in range(6):
+                color += random.choice("0123456789abcdef")
+            colors.append(color)
+
+    elif color_type == "rgb":
+        for i in range(n):
+            colors.append(
+                f"rgb({random.randint(0,255)}, {random.randint(0,255)}, {random.randint(0,255)})"
+            )
+
+    return colors
+
+print(generate_colors("hexa",3))
+print(generate_colors("rgb",3))
