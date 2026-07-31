d9f2cd06fba55f663579e3d7845dce3f70425759
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-31 19:56:52 +0530

    Learn about filter() function with example

--- Day 14- Higher Order Function/Bulit-in_higher_function.py
+++ Day 14- Higher Order Function/Bulit-in_higher_function.py
@@ -27,3 +27,38 @@ def change_to_upper(name):
 
 names_upper_cased = map(change_to_upper, names)
 print(list(names_upper_cased))    # ['ASABENEH', 'LIDIYA', 'ERMIAS', 'ABRAHAM']
+
+
+"Python - Filter Function"
+Example:1
+
+# Lets filter only even nubers
+numbers = [1, 2, 3, 4, 5]  # iterable
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+even_numbers = filter(is_even, numbers)
+print(list(even_numbers))       # [2, 4]
+Example:2
+
+numbers = [1, 2, 3, 4, 5]  # iterable
+
+def is_odd(num):
+    if num % 2 != 0:
+        return True
+    return False
+
+odd_numbers = filter(is_odd, numbers)
+print(list(odd_numbers))       # [1, 3, 5]
+# Filter long name
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']  # iterable
+def is_name_long(name):
+    if len(name) > 7:
+        return True
+    return False
+
+long_names = filter(is_name_long, names)
+print(list(long_names))         # ['Asabeneh']
