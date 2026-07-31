1834583a284595caabdca3aa7e6c78987770746c
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-31 19:49:22 +0530

    Applying multiple decorators in single parameter

--- Day 14- Higher Order Function/higher_order_function.py
+++ Day 14- Higher Order Function/higher_order_function.py
@@ -74,3 +74,30 @@ def uppercase_decorator(function):
 def greeting():
     return 'Welcome to Python'
 print(greeting())   # WELCOME TO PYTHON
+
+#Applying Multiple Decorators to a Single Function
+'''These decorator functions are higher order functions
+that take functions as parameters'''
+
+# First Decorator
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    return wrapper
+
+# Second decorator
+def split_string_decorator(function):
+    def wrapper():
+        func = function()
+        splitted_string = func.split()
+        return splitted_string
+    return wrapper
+
+#Decorators will be executed from bottom to top
+@split_string_decorator
+@uppercase_decorator     # order with decorators is important in this case - .upper() function does not work with lists
+def greeting():
+    return 'Welcome to Python'
+print(greeting())   # ['WELCOME', 'TO', 'PYTHON']
