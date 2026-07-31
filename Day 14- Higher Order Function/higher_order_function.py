e87d1362725cc04acaae8d10020b47a84028e894
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-07-31 19:48:22 +0530

    Python creating decorators

--- Day 14- Higher Order Function/higher_order_function.py
+++ Day 14- Higher Order Function/higher_order_function.py
@@ -46,3 +46,31 @@ def add_ten():
 closure_result = add_ten()
 print(closure_result(5))  # 15
 print(closure_result(10))  # 20
+
+#Python Decorators example:
+# Normal function
+def greeting():
+    return 'Welcome to Python'
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    return wrapper
+g = uppercase_decorator(greeting)
+print(g())          # WELCOME TO PYTHON
+
+## Let us implement the example above with a decorator
+
+'''This decorator function is a higher order function
+that takes a function as a parameter'''
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+    return wrapper
+@uppercase_decorator
+def greeting():
+    return 'Welcome to Python'
+print(greeting())   # WELCOME TO PYTHON
