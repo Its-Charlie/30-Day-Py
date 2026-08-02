d5cf7c366afac7e20199b0de251922d5d9cca12e
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-02 19:47:49 +0530

    Create Date & Time

--- Day 16-Datetime/DateTime.py
+++ Day 16-Datetime/DateTime.py
@@ -1,2 +1,13 @@
 import datetime
 from datetime import datetime, date, time, timedelta
+
+#Current Date & Time
+now = datetime.now()
+
+now.day
+now.month
+now.year
+now.hour
+now.minute
+now.second
+now.timestamp()
