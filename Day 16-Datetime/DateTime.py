f99a19967b021b8a6f72c7089d501d7598f77bb3
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-02 19:49:16 +0530

    Miscellous

--- Day 16-Datetime/DateTime.py
+++ Day 16-Datetime/DateTime.py
@@ -1,13 +1,2 @@
 import datetime
 from datetime import datetime, date, time, timedelta
-
-#Current Date & Time
-now = datetime.now()
-
-now.day
-now.month
-now.year
-now.hour
-now.minute
-now.second
-now.timestamp()
