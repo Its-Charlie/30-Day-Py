99d93ca136b8657c12132b9838b162d1bd8b03cd
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-03 21:57:06 +0530

    Exception Simple example

---
+++ Day 17- Exception/Exception.py
@@ -0,0 +1,10 @@
+#Specific Exceptions
+try:
+    # code
+    pass
+except TypeError:
+    print("Type Error")
+except ValueError:
+    print("Value Error")
+except ZeroDivisionError:
+    print("Division by Zero")
