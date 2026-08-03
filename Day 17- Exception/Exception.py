2d00515b14d401e1c4b357c60372125acf05e7d5
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-03 21:58:19 +0530

    Using Else in exception

--- Day 17- Exception/Exception.py
+++ Day 17- Exception/Exception.py
@@ -8,3 +8,12 @@ except ValueError:
     print("Value Error")
 except ZeroDivisionError:
     print("Division by Zero")
+
+'else => Runs only if no exception occurs.'
+
+try:
+    x = 10 / 2
+except:
+    print("Error")
+else: 
+    print("Success")
