9cd65af6f30f942d1e2650c24b9e317ce2c45fe8
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-04 19:14:11 +0530

    Using search()

--- Day 18- Regular Expressions/RE.py
+++ Day 18- Regular Expressions/RE.py
@@ -1,9 +1,20 @@
 #Import
 import re
 
-'''re.match()
+'''Syntax of re.match():
 re.match(pattern, text)'''
 
 text = "Python is awesome"
 
 print(re.match("Python", text))
+
+'''re.search()
+
+Finds first occurrence.
+
+re.search(pattern, text)
+'''
+
+text = "I love Python"
+
+print(re.search("Python", text))
