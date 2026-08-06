96f2a971c428316080dcb61db664f051145e5db6
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-06 22:21:06 +0530

    Import all define function from arithematic.py to __init__.py file

---
+++ Day 20- Python Package Manager/__init__.py
@@ -0,0 +1,8 @@
+import arithmetic
+
+print(arithmetic.add_numbers(1,2,3))
+print(arithmetic.subtract(5,3))
+print(arithmetic.multiple(2,4))
+print(arithmetic.division(10,2))
+print(arithmetic.remainder(10,3))
+print(arithmetic.power(2,3))
