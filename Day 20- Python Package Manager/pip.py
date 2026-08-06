652b05dd014145a52ce728818dadaf99e352700c
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-06 21:49:53 +0530

    successfully install and run numpy

---
+++ Day 20- Python Package Manager/pip.py
@@ -0,0 +1,10 @@
+# using numpy after installing it with pip
+'''before running this code, make sure to install numpy using pip:
+pip insrtall numpy"'''
+
+import numpy as np
+arr = np.array([1,2,3])
+
+print(arr)
+print(arr+2)
+print(arr*5)
