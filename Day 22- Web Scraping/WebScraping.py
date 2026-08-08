5b57d8b2894e0be192326c56c2b0582e65a7f6d0
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-08 15:26:04 +0530

    Get page info

--- Day 22- Web Scraping/WebScraping.py
+++ Day 22- Web Scraping/WebScraping.py
@@ -13,3 +13,7 @@ response = requests.get(url)
 print(response.status_code)
 
 soup = BeautifulSoup(response.content, "html.parser")
+
+print(soup.title)
+print(soup.title.get_text())
+print(soup.body)
