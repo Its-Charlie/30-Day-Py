fa43e0430a3957365b2ad7f67e480c2b9ef961e9
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-08 15:30:25 +0530

    Get find by tag and attribute

--- Day 22- Web Scraping/WebScraping.py
+++ Day 22- Web Scraping/WebScraping.py
@@ -20,4 +20,9 @@ print(soup.body)
 
 #Find HTML elements:
 
+'Find all the tables in the HTML document'
 tables = soup.find_all("table")
+
+'Find by tag + attribute:'
+
+tables = soup.find_all("table", {"cellpadding": "3"})
