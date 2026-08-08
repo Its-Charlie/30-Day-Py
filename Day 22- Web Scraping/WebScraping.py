253621bd5f2b9a530afe804f564f4733ab23c28b
Siddhesh Pramod Yadav <siddheshy39@gmail.com>
2026-08-08 15:25:33 +0530

    Basic workflow

--- Day 22- Web Scraping/WebScraping.py
+++ Day 22- Web Scraping/WebScraping.py
@@ -3,3 +3,13 @@ Install packages
 pip install requests
 pip install beautifulsoup4
 '''
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+
+response = requests.get(url)
+
+print(response.status_code)
+
+soup = BeautifulSoup(response.content, "html.parser")
